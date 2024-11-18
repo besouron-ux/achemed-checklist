0f2173aaa032e8ced6bb1e6c8c8e6e9e0d52876a
Paulo <paulinop377@gmail.com>
2024-11-18 17:20:10 -0300

    api's testadas pelo postman

--- achemed_checklist/achemed_checklist/urls.py
+++ achemed_checklist/achemed_checklist/urls.py
@@ -14,18 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
 from django.contrib import admin
-from django.urls import path, include
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from checklist.views import ChecklistViewSet
 
 router = DefaultRouter()
-router.register(r'checklists', ChecklistViewSet)
+router.register("checklists", ChecklistViewSet)
+
+def home(request):
+    return HttpResponse("Bem-vindo à API do Achemed Checklist!")
 
 urlpatterns = [
+    path('', home),  # Página inicial
     path('admin/', admin.site.urls),
-    path('api/', include('checklist.urls')),
+    path('api/', include(router.urls)),  # Rotas da API
 ]
 
 
